8c40310c87bb33ab64d8ac5e90bf6297fb6ce48d
If-smile <127843080+If-smile@users.noreply.github.com>
2025-03-10 19:04:46 +0000

    Update steam_to_csv.py

--- steam_to_csv.py
+++ steam_to_csv.py
@@ -2,8 +2,8 @@ import requests
 import csv
 
 # 请将下面的 API_KEY 和 STEAM_ID 替换成你自己的
-API_KEY = 'DB47FF9CA53B2DE243CD8D8B56F6D7F2'
-STEAM_ID = '76561199339956907'
+API_KEY = ''
+STEAM_ID = ''
 
 # 构造 API 请求 URL，include_appinfo=1 表示返回游戏名称信息
 url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={API_KEY}&steamid={STEAM_ID}&format=json&include_appinfo=1"
